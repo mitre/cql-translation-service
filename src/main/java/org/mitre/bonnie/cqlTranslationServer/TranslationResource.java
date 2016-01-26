9d9a8730bb5192b5aa96d839b21ec88f0e506e63
Marc Hadley <mhadley@mitre.org>
2016-01-26 17:26:18 -0500

    Started adding cql-to-elm dependencies and POST endpoint

--- src/main/java/org/mitre/bonnie/cqlTranslationServer/TranslationResource.java
+++ src/main/java/org/mitre/bonnie/cqlTranslationServer/TranslationResource.java
@@ -1,6 +1,9 @@
 package org.mitre.bonnie.cqlTranslationServer;
 
+import java.io.InputStream;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -21,5 +24,12 @@ public class TranslationResource {
   @Produces(MediaType.TEXT_PLAIN)
   public String getIt() {
     return "Got it!";
+  }
+  
+  @POST
+  @Consumes("application/cql")
+  @Produces("application/elm+xml")
+  public String translate(InputStream cql) {
+    return "working on it";
   }
 }
