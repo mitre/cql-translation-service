0b7edc4467f1e47d66c479b17a4cc56e50210118
Marc Hadley <mhadley@mitre.org>
2016-02-03 17:56:14 -0500

    Support ELM-JSON and translation errors reported as part of the converted library

--- src/main/java/org/mitre/bonnie/cqlTranslationServer/TranslationResource.java
+++ src/main/java/org/mitre/bonnie/cqlTranslationServer/TranslationResource.java
@@ -2,24 +2,17 @@ package org.mitre.bonnie.cqlTranslationServer;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 import org.cqframework.cql.cql2elm.CqlTranslator;
-import org.cqframework.cql.cql2elm.CqlTranslatorException;
-import org.cqframework.cql.cql2elm.DefaultLibrarySourceProvider;
-import org.cqframework.cql.cql2elm.LibrarySourceLoader;
-import org.cqframework.cql.elm.tracking.TrackBack;
+import org.cqframework.cql.cql2elm.CqlTranslator.Options;
 
 /**
  * Root resource (exposed at "translator" path)
@@ -42,20 +35,41 @@ public class TranslationResource {
   @POST
   @Consumes("application/cql")
   @Produces("application/elm+xml")
-  public String translate(File cql) {
+  public Response cqlToElmXml(File cql) {
     try {
       //LibrarySourceLoader.registerProvider(
       //        new DefaultLibrarySourceProvider(cql.toPath().getParent()));
-      // TODO: Add options ?
-      CqlTranslator translator = CqlTranslator.fromFile(cql);
+      Options options[] = {Options.EnableAnnotations};
+      CqlTranslator translator = CqlTranslator.fromFile(cql, options);
       //LibrarySourceLoader.clearProviders();
       
-      if (translator.getErrors().size() > 0) {
-        throw new TranslationFailureException(translator.getErrors());
-      }
-      return translator.toXml();
+      ResponseBuilder resp = translator.getErrors().size() > 0 ? 
+              Response.status(Status.BAD_REQUEST) : Response.ok();
+      resp = resp.entity(translator.toXml()).type("application/elm+xml");
+      return resp.build();
     } catch (IOException ex) {
       throw new TranslationFailureException("Unable to read request");
     }
   }
+
+  @POST
+  @Consumes("application/cql")
+  @Produces("application/elm+json")
+  public Response cqlToElmJson(File cql) {
+    try {
+      //LibrarySourceLoader.registerProvider(
+      //        new DefaultLibrarySourceProvider(cql.toPath().getParent()));
+      Options options[] = {Options.EnableAnnotations};
+      CqlTranslator translator = CqlTranslator.fromFile(cql, options);
+      //LibrarySourceLoader.clearProviders();
+      
+      ResponseBuilder resp = translator.getErrors().size() > 0 ? 
+              Response.status(Status.BAD_REQUEST) : Response.ok();
+      resp = resp.entity(translator.toJson()).type("application/elm+xml");
+      return resp.build();
+    } catch (IOException ex) {
+      throw new TranslationFailureException("Unable to read request");
+    }
+  }
+
 }
